4c635d8dc59c45324b7b10290b54be8e8cc45e20
Chawit Sakulyuenyong <Chawit.Sky@hotmail.com>
2019-05-11 18:52:16 +0700

    Add koaBodyParser

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const utilRepo = require('./my-util');
 const koa = require('koa');
+const koaBodyParser = require('koa-bodyparser');
 const koaRouter = require('koa-router');
 
 const app = new koa();
 const router = new koaRouter();
 const port = 3000;
 
-router.get('/ping', (ctx, next) => {
+const utilRepo = require('./my-util');
+const allRoomRepo = require('./all-room');
+
+router.get('/ping', ctx => {
   utilRepo.write('test.txt', 'pong');
   ctx.response.body = utilRepo.read('test.txt');
 });
- 
-app.use(router.routes())
+
+app.use(koaBodyParser());
+app.use(router.routes());
 app.listen(port);
